c9aa8fadcc5d063a76adba95326cf82162491b08
peepboy <peepboy@gmail.com>
2026-03-14 20:17:44 +0900

    Initial Commit

---
+++ LangSwitcher/LangSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LangSwitcherApp.swift
+//  LangSwitcher
+//
+//  Created by GO Wonseok Mini on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct LangSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
